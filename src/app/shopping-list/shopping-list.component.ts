31600dc78a802819f1e5ced66840eda1cfc1144b
actionanand <meetanandns@gmail.com>
2019-10-21 11:29:25 +0530

    shopping list basic -html

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../models/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple',5),
+    new Ingredient('tomatto',3)
+  ];
 
   constructor() { }
 
